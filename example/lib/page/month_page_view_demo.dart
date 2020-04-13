7dc2bee0ca17653b41a86d8bbdd8b714edbaca15
yshye <yshye824252187@163.com>
2020-04-13 18:27:58 +0800

    支持多选

--- example/lib/page/month_page_view_demo.dart
+++ example/lib/page/month_page_view_demo.dart
@@ -11,38 +11,24 @@ class _MonthPageViewDemoState extends State<MonthPageViewDemo> {
   MonthPageController _controller;
   DateMonth _month = DateMonth.now();
   DateDay _firstDay, _secondDay, _day;
-  bool _checked = false;
+  List<DateDay> _days = [];
+  int _selectType = 1;
   String _data = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('翻页日历'),
-        centerTitle: true,
-        actions: <Widget>[
-          Switch(
-            value: _checked,
-            activeColor: Colors.white,
-            onChanged: (select) {
-              _checked = select;
-              if (_controller != null)
-                _controller
-                  ..setEnableContinuous(_checked)
-                  ..reLoad();
-              setState(() {});
-            },
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('翻页日历'), centerTitle: true),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            _buildSelectTime(),
             MonthPageView(
               padding: EdgeInsets.all(1),
               scrollDirection: Axis.horizontal,
               option: MonthOption(
-                enableContinuous: _checked,
+                enableContinuous: _selectType == 2,
                 marks: {
                   DateDay.now().copyWith(day: 1): '111',
                   DateDay.now().copyWith(day: 5): '222',
@@ -50,13 +36,18 @@ class _MonthPageViewDemoState extends State<MonthPageViewDemo> {
                   DateDay.now().copyWith(day: 19): '444',
                   DateDay.now().copyWith(day: 26): '444',
                 },
-                minDay: DateDay.now().copyWith(month: 8, day: 13),
+                minDay: DateDay.now().copyWith(month: 1, day: 13),
                 maxDay: DateDay.now().copyWith(month: 12, day: 21),
+                enableMultiple: _selectType == 3,
               ),
               showWeekHead: true,
               onContinuousSelectListen: (firstDay, endFay) {
                 _firstDay = firstDay;
                 _secondDay = endFay;
+                setState(() {});
+              },
+              onMultipleSelectListen: (list) {
+                _days = list;
                 setState(() {});
               },
               onMonthChange: (month) {
@@ -89,13 +80,36 @@ class _MonthPageViewDemoState extends State<MonthPageViewDemo> {
             Container(
               child: Text('''当前月份：${_month.toString(yearSuffix: '年', monthSuffix: '月')}
 可选范围：${DateDay.now().copyWith(month: 8, day: 13)} 至  ${DateDay.now().copyWith(month: 12, day: 21)} 
-选择模式：${_checked ? '连选' : '单选'} 
-单选日期：${_day?.toString(yearSuffix: '年', monthSuffix: '月', daySuffix: '日') ?? ''}   ::   ${_data ?? ''}
+选择模式：${_selectType == 2 ? '连选' : _selectType == 3 ? '多选' : '单选'} 
+单选日期：${_day?.toString(yearSuffix: '年', monthSuffix: '月', daySuffix: '日') ?? ''}   Mark： ${_data ?? ''}
 连选日期：${_firstDay?.toString(yearSuffix: '年', monthSuffix: '月', daySuffix: '日') ?? ''}  至  ${_secondDay?.toString(yearSuffix: '年', monthSuffix: '月', daySuffix: '日') ?? ''}
+连续日期：${_days.join('，')}
 '''),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSelectTime() {
+    Map<int, Widget> map = {
+      1: Container(width: 40, alignment: Alignment.center, child: Text('单选')),
+      2: Container(width: 40, alignment: Alignment.center, child: Text('连续')),
+      3: Container(width: 40, alignment: Alignment.center, child: Text('多选')),
+    };
+    return Container(
+      alignment: Alignment.topRight,
+      child: CupertinoSegmentedControl<int>(
+        onValueChanged: (index) {
+          _selectType = index;
+          _controller?.setEnableContinuous(_selectType == 2);
+          _controller?.setEnableMultiple(_selectType == 3);
+          _controller.reLoad();
+          setState(() {});
+        },
+        children: map,
+        groupValue: _selectType,
       ),
     );
   }
